b555f3be07e97df6b1e9147ce31b61bf9885653f
iaman26 <anlengocc5@gmail.com>
2025-03-01 21:41:37 +0700

    update

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex">
-    <img src="/src/assets/images/banner-login.png" class="w-2/3" alt="" />
-    <div class="dark:bg-gray-900 w-full">
+  <div class="flex w-full justify-center">
+    <img src="/src/assets/images/banner-login.png" class="w-full" alt="" />
+    <div class="dark:bg-gray-900 w-[448px]">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div
-          class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+          class="w-[448px] rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
         >
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1
@@ -86,9 +86,9 @@
                 Login
               </button>
 
-              <div class="flex items-center w-full mt-5 justify-between">
+              <div class="flex login-with items-center w-full mt-5 justify-between">
                 <button
-                  class="py-2 border flex gap-2 border-slate-200 h-10 px-3 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+                  class="py-2 border button flex gap-2 border-slate-200 justify-center h-10 px-3 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
                 >
                   <img
                     class="ml-4 w-4 h-4"
@@ -99,7 +99,7 @@
                   <span class="pr-4 text-sm">Login with Google</span>
                 </button>
                 <button
-                  class="py-2 border flex gap-2 border-slate-200 h-10 px-3 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
+                  class="py-2 button border flex gap-2 border-slate-200 justify-center mt-5 xl:mt-0 h-10 px-3 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150"
                 >
                   <img
                     class="ml-4 w-4 h-4"
@@ -178,4 +178,19 @@ export default defineComponent({
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.login-with {
+  flex-direction: column;
+}
+.button {
+  margin-top: 12px;
+}
+@media screen and (min-width: 1280px) {
+  .login-with {
+    flex-direction: row;
+  }
+  .button {
+    margin-top: 0;
+  }
+}
+</style>
